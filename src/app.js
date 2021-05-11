b753b6346f4d2ffae24c6012b432df668510acaf
freilinjb <freilinjb@gmail.com>
2021-05-10 22:44:58 -0400

    Subiendo imagenes a la api

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ app.set("pkg", pkg);
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use("/public", express.static('src/uploads'));
 
 // Welcome Routes
 app.get("/", (req, res) => {
@@ -34,7 +36,7 @@ app.get("/", (req, res) => {
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,9 +1,44 @@
 const router = require('express').Router();
+const multer = require('multer');
 
 const { checkToken } = require('../auth/token_validation');
 const product = require('../controllers/product.controller');
 
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, './src/uploads/img/product');
+    },
+    filename: function(req, file, cb) {
+      cb(null, `${Math.random(0,1).toString().substr(2)}${file.originalname}`);
+    }
+  });
 
+  const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+
+  const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+  });
+
+router.post("/uploads", upload.single('productImag'), (req, res, next) => {
+    try {
+        console.log('asdfa');
+    } catch (error) {
+        console.log(error);
+    }
+
+    return res.status(200).json("234");
+});
 
 router.get(
     "/product/category",
