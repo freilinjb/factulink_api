8dc6ab2256b5cf0b1975270b31c1b62e422798a5
freilinjb <freilinjb@gmail.com>
2021-06-11 18:35:07 -0400

    Paginando las subcategorias

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -256,14 +256,44 @@ exports.deleteCategory = async (req, res) => {
  * ENDPOINT SUBCATEGORISA START
  */
 exports.getSubCategory = (req, res) => {
-  product.getSubCategory((error, results) => {
-    if (error) {
+  let idSubCategoria = null;
+  let data = {};
+
+  idSubCategoria = req.params.idSubCategoria ? req.params.idSubCategoria : null;
+  //const prueba = req.query.prueba;
+  console.log("req: prueba: ", req.query.page);
+  data.idSubCategoria = idSubCategoria;
+  data.page = req.query.page;
+  data.search = req.query.search;
+  data.limit = req.query.limit;
+  console.log("data: ", data);
+  if (!data.limit) {
+    data.limit = 20;
+  }
+  data.offset = (data.page - 1) * data.limit;
+  product.getSubCategory(data, (err, results, total_page, total_rows) => {
+    if (err) {
+      console.log("error: ", err);
       return res.status(500).json({
         error: 1,
         success: 0,
         msg: "Ah ocurrido un error interno",
       });
     }
+
+    if (data.page) {
+      return res.status(200).json({
+        success: 1,
+        data: {
+          total_page: Math.ceil(total_page),
+          page_cout: results.length,
+          page_number: Number(data.page),
+          total_rows: Number(total_rows),
+          results: results,
+        },
+      });
+    }
+
     return res.status(200).json({
       success: 1,
       data: results,
