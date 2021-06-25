c046aadf9e3d76810e59ad88f7f45d455f85fa30
freilinjb <freilinjb@gmail.com>
2021-06-25 19:51:13 -0400

    Optimizacion

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,23 +2,19 @@ const { connection } = require("../config/database");
 
 exports.getEmployees = async (data, callback) => {
   try {
-    let condicion = '';
-    let search = '';
+    let condicion = "";
+    let search = "";
 
-    
-    condicion = data.idUsuario ? `WHERE u.idUsuario = ${data.idUsuario}`   : "";
+    condicion = data.idUsuario ? `WHERE idUsuario = ${data.idUsuario}` : "";
     if (data.idUsuario == null && data.limit && data.offset >= 0) {
-
       //Valida si es una busqueda que se va arelizar
-    if(data.search) {
-
-      data.search = data.search.trim();
-      search = ` WHERE concat(nombre, sexo, correo, telefono, usuario, tipoUsuario, estado ) LIKE '%${data.search}%' `; 
-    }
+      if (data.search) {
+        data.search = data.search.trim();
+        search = ` WHERE concat(nombre, sexo, correo, telefono, usuario, tipoUsuario, estado ) LIKE '%${data.search}%' `;
+      }
       condicion += ` LIMIT ${data.limit}  OFFSET ${data.offset} `;
     }
 
-    
     let total_page = 0;
     let total_rows = 0;
 
@@ -26,16 +22,15 @@ exports.getEmployees = async (data, callback) => {
       `SELECT COUNT(v.idUsuario) AS cantidad FROM empleado_v v ${search}`,
       [],
       async (error, results, fields) => {
-       // total_page = results[0].cantidad;
-       if(results.length > 0) {
-         console.log('prueba: ', results);
-        total_rows = results[0].cantidad;
-       } else {
-        total_rows = 0;
-        console.log('length: ', results);
-
-       }
-        console.log('cantidad total: ', total_rows);
+        // total_page = results[0].cantidad;
+        if (results.length > 0) {
+          console.log("prueba: ", results);
+          total_rows = results[0].cantidad;
+        } else {
+          total_rows = 0;
+          console.log("length: ", results);
+        }
+        console.log("cantidad total: ", total_rows);
         connection.query(
           `SELECT 
               idUsuario, 
@@ -51,9 +46,10 @@ exports.getEmployees = async (data, callback) => {
           `,
           [],
           (error, results, fields) => {
-            console.log('idUser: ', Math.ceil(total_rows / data.limit));
-            
-            total_page = data.idUsuario == null ? Math.ceil(total_rows / data.limit) : 1;
+            console.log("idUser: ", Math.ceil(total_rows / data.limit));
+
+            total_page =
+              data.idUsuario == null ? Math.ceil(total_rows / data.limit) : 1;
             console.log("total_page: ", total_rows);
 
             return error
@@ -65,7 +61,6 @@ exports.getEmployees = async (data, callback) => {
         return total_page;
       }
     );
-
   } catch (error) {
     console.log(error);
   }
@@ -103,35 +98,34 @@ exports.getEmployee = async (idEmpleado, callback) => {
 };
 
 exports.addUser = async (data, callback) => {
-    connection.query(
-      "CALL registrarEmpleado (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-      [
-        data.nombre,
-        data.apellido,
-        data.idSexo,
-        data.idTipoIdentificacion,
-        data.identificacion,
-        data.fechaNacimiento,
-        data.correo,
-        data.telefono,
-        data.usuario,
-        data.clave,
-        data.idProvincia,
-        data.idCiudad,
-        data.direccion,
-        data.idTipoUsuario,
-        data.creado_por
-      ],
-      (error, result, fields) => {
-        console.log("error");
-        return error ? callback(error) : callback(null, result);
-      }
-    );
-  };
+  connection.query(
+    "CALL registrarEmpleado (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+    [
+      data.nombre,
+      data.apellido,
+      data.idSexo,
+      data.idTipoIdentificacion,
+      data.identificacion,
+      data.fechaNacimiento,
+      data.correo,
+      data.telefono,
+      data.usuario,
+      data.clave,
+      data.idProvincia,
+      data.idCiudad,
+      data.direccion,
+      data.idTipoUsuario,
+      data.creado_por,
+    ],
+    (error, result, fields) => {
+      console.log("error");
+      return error ? callback(error) : callback(null, result);
+    }
+  );
+};
 
 exports.updateProduct = async (data, callback) => {
-
-    console.log('data2: ', data);
+  console.log("data2: ", data);
 
   connection.query(
     "CALL registrarProducto (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
